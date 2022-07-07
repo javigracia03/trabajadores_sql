4dbcb48be88ac4ee745da483db29a556c079a0f4
javiergracia03 <javiergracia2003@gmail.com>
2022-07-07 21:28:13 +0200

    v1

---
+++ src/datos_trabajadores/Datos_trabajadores.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package datos_trabajadores;
+
+/**
+ *
+ * @author javie
+ */
+public class Datos_trabajadores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
