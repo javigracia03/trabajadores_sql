6891d65007a9e55a2057f2215eee139d65df62e6
javiergracia03 <javiergracia2003@gmail.com>
2022-07-08 11:10:12 +0200

    class Persona added

--- src/datos_trabajadores/Persona.java
+++ src/datos_trabajadores/Persona.java
@@ -10,4 +10,55 @@ package datos_trabajadores;
  */
 public class Persona {
     
-}
+    private static String nombre;
+    private static int dni;
+    private int horas= 0;
+    private static String apellidos;
+    
+    public static void setdni(int num){
+
+        dni = num;
+
+    }//()
+    
+    public static void setnombre(String nom){
+
+        nombre = nom;
+
+    }//()    
+    
+    public static void setapellidos(String surname){
+
+        apellidos = surname;
+
+    }//()    
+    
+    public void add_horas(int horas_anyadidas){
+
+        horas = horas + horas_anyadidas;
+
+    }//()        
+    
+    
+    public String getnombre(){
+    
+      return this.nombre;
+    
+    }//()
+    
+    public String getapellidos(){
+    
+        return this.apellidos;
+    }//()
+    
+    public int getdni(){
+        
+        return this.dni;
+    }//()
+    
+    public int gethoras(){
+    
+        return this.horas;
+    }//()
+    
+}// class
